2809a99d0ac3d2cd7855b6c070ea110fe8077eb6
Sonal Pawar <111594216+pawarsonal01998@users.noreply.github.com>
2022-08-26 16:58:19 +0530

    Add files via upload

---
+++ Addi.java
@@ -0,0 +1,16 @@
+package firstPoject;
+
+public class Addi 
+{
+	 public static void main(String[] args)
+	 {
+	  int i=1, n=0;
+	  while(i<=10)
+	       {
+	         n += i;
+	         i++;
+	       }
+	  System.out.print("Addi:"+n);
+	 } 
+	}
+
